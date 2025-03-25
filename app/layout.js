4117166c8780e5a3df223545e515325f826d906b
thesilvrwolfx <2023ucp1571@mnit.ac.in>
2025-03-25 03:30:56 +0000

    added signup(not working)

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import StyledComponentsRegistry from "@/app/lib/AntRegistry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,10 +20,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <StyledComponentsRegistry>
         {children}
+          </StyledComponentsRegistry>
+        
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- contributions.md
+++ contributions.md
@@ -1,1 +1,2 @@
 22 March, 2025: @silvrwolfx created this repository, and added the contributions.md file, and a readme.md file.
+22 March, 2025: @silvrwolfx added the signup using clerk.js
